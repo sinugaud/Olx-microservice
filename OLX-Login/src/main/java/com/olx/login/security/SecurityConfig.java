fe3787f933b5b2b312c6860505e773aa8005ede3
Shailendra Goad <63011567+sinugaud@users.noreply.github.com>
2022-06-02 11:45:25 +0530

    Delete OLX-Login directory

--- OLX-Login/src/main/java/com/olx/login/security/SecurityConfig.java
+++
@@ -1,7 +0,0 @@
-package com.olx.login.security;
-
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-}
